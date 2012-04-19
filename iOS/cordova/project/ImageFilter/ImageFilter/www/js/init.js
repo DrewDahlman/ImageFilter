745473a189fc4553d1b6c320fa612e8ce9d2d797
Drew Dahlman <info@drewdahlman.com>
2012-04-18 21:40:56 -0600

    Example xCode Project

---
+++ iOS/cordova/project/ImageFilter/ImageFilter/www/js/init.js
@@ -0,0 +1,102 @@
+// PROJECT: ImageFilter Phonegap Plugin
+// AUTHOR: Drew Dahlman (www.drewdahlman.com)
+// DATE: 2.25.2012
+// VERSION: 1.0
+/*
+NOTES:
+A few things to read before using this plugin.
+Each filter is created with objective-c. Thus modifications to effects must be made there.
+Applying a filter is simple - 
+
+EXAMPLE:
+plugins.ImageFilter.stark({
+    image:imageURI,
+    save:'false'
+});
+ 
+*/
+
+// GLOBAL VARS
+var largeImage;
+
+var app = {
+    bodyLoad: function () {
+        document.addEventListener("deviceready", app.deviceReady, false);
+    },
+    deviceReady: function () {
+        app.init();
+    },
+    init: function () {
+		
+    },
+    useCamera: function () {
+        navigator.camera.getPicture(app.onCameraSuccess, app.onCameraFail, {
+            quality: 100,
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: Camera.PictureSourceType.CAMERA,
+            //allowEdit : true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 910,
+            targetHeight: 910,
+            saveToPhotoAlbum: false
+        });
+    },
+    useRoll: function () {
+        navigator.camera.getPicture(app.onCameraSuccess, app.onCameraFail, {
+            quality: 100,
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 910,
+            targetHeight: 910,
+            saveToPhotoAlbum: false
+        });
+    },
+    onCameraSuccess: function (imageURI) {
+        
+		largeImage = imageURI;
+		$(".photo").html("<img src='" + imageURI + "'>");
+		$(".photo").show();
+		
+    },
+    onCameraFail: function (msg) {
+        console.log("ERROR! -" + msg);
+    }
+};
+
+var filters = {
+    none: function (imageURI) {
+        plugins.ImageFilter.none(filters.rendered, {
+            image: imageURI,
+            save: 'false',
+        });
+    },
+    sunnySide: function (imageURI) {
+        plugins.ImageFilter.sunnySide(filters.rendered, {
+            image: imageURI,
+            save: 'false'
+        });
+    },
+    worn: function (imageURI) {
+        plugins.ImageFilter.worn(filters.rendered, {
+            image: imageURI,
+            save: 'false'
+        });
+    },
+    vintage: function (imageURI) {
+        plugins.ImageFilter.vintage(filters.rendered, {
+            image: imageURI,
+            save: 'false'
+        });
+    },
+    stark: function (imageURI) {
+        plugins.ImageFilter.stark(filters.rendered, {
+            image: imageURI,
+            save: 'false'
+        });
+    },
+    rendered: function (msg) {
+		
+        $(".photo").html("<img src='" + msg + "'>");
+    }
+}
---
+++ iOS/cordova/project/ImageFilter/ImageFilter/www/js/plugins/ImageFilter.js
@@ -0,0 +1,68 @@
+/*
+Copyright (c) 2012 Drew Dahlman MIT LICENSE
+*/
+
+var ImageFilter = function () {};
+
+ImageFilter.prototype.clean = function (options) {
+
+    cordova.exec("ImageFilter.clean");
+};
+ImageFilter.prototype.none = function (done,options) {
+    //console.log(options+" "+done);
+    var defaults = {
+        image: '',
+        save: '',
+    };
+    for(var key in defaults) {
+        if(typeof options[key] !== "undefined") defaults[key] = options[key];
+    }
+
+    return cordova.exec(done,null,"ImageFilter","none",[defaults]);
+};
+ImageFilter.prototype.sunnySide = function (done,options) {
+    var defaults = {
+        image: '',
+        save: '',
+    };
+    for(var key in defaults) {
+        if(typeof options[key] !== "undefined") defaults[key] = options[key];
+    }
+    return cordova.exec(done,null,"ImageFilter","sunnySide",[defaults]);
+};
+ImageFilter.prototype.worn = function (done,options) {
+    var defaults = {
+        image: '',
+        save: '',
+    };
+    for(var key in defaults) {
+        if(typeof options[key] !== "undefined") defaults[key] = options[key];
+    }
+   	return cordova.exec(done,null,"ImageFilter","worn",[defaults]);
+};
+ImageFilter.prototype.vintage = function (done,options) {
+    var defaults = {
+        image: '',
+        save: '',
+    };
+    for(var key in defaults) {
+        if(typeof options[key] !== "undefined") defaults[key] = options[key];
+    }
+    return cordova.exec(done,null,"ImageFilter","vintage",[defaults]);
+};
+ImageFilter.prototype.stark = function (done,options) {
+    var defaults = {
+        image: '',
+        save: '',
+    };
+    for(var key in defaults) {
+        if(typeof options[key] !== "undefined") defaults[key] = options[key];
+    }
+    return cordova.exec(done,null,"ImageFilter","stark",[defaults]);
+};
+cordova.addConstructor(function () {
+    if(!window.plugins) {
+        window.plugins = {};
+    }
+    window.plugins.ImageFilter = new ImageFilter();
+});
